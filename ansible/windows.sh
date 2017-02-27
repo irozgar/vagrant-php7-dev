dc57093499560bbb020298ca3019f2966b381795
irozgar <irozgar@gmail.com>
2017-02-27 19:18:39 +0100

    Use ansible 2 to do the deployment

--- ansible/windows.sh
+++ ansible/windows.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 # Update Repositories
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install -y software-properties-common ansible
+# Add ansible repo only if its not already present
+ansibleRepoAdded=`grep ppa.launchpad.net/ansible /etc/apt/sources.list | wc -l`
+
+if [ $ansibleRepoAdded -eq 0 ]; then
+    echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    sudo apt-get update -y
+fi
+
+sudo apt-get install ansible -y
 
 
 cp /vagrant/ansible/inventories/dev /etc/ansible/hosts -f
